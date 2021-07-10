72f5eba83677c4775c281ad3ea8c4150b85fbe28
rom <3103rom@gmail.com>
2021-07-10 12:19:15 +0900

    first commit

---
+++ src/components/ChatBoard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+      {{ json }}
+  </v-container>
+</template>
+
+<script>
+    import firebase from 'firebase'
+
+    export default {
+        name: 'ChatBoard',
+        data: () => ({
+            json: {}
+        }),
+        created() {
+            const dbRef = firebase.database().ref("app");
+                dbRef.on('value', (snapshot) => {
+                this.json = snapshot.val();
+            });
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
+const config = {
+  apiKey: "AIzaSyB3-H56BndEn2DY2oQCe7UN5qkQAiehs88",
+  authDomain: "kdghacks-team-d.firebaseapp.com",
+  databaseURL: "https://kdghacks-team-d-default-rtdb.firebaseio.com",
+  projectId: "kdghacks-team-d",
+  storageBucket: "kdghacks-team-d.appspot.com",
+  messagingSenderId: "868270110711",
+  appId: "1:868270110711:web:d33b399fe08bf8625020f2",
+  measurementId: "G-NLHDTJDZ09"
+};
+
+firebase.initializeApp(config)
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
